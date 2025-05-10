34d09c39191a97f7f7df70c2693971443f340dd4
Paz Marx <paz.marx@gmail.com>
2025-05-10 09:56:30 +0300

    Security: Remove actual API key and use placeholder

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuration file - This file should be copied to config.local.js and modified with your actual keys
+// DO NOT commit config.local.js to version control
+
+const config = {
+    UPLOADCARE_PUBLIC_KEY: 'YOUR_PUBLIC_KEY' // Replace with your actual Uploadcare public key
+}; 
